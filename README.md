327b6b0dfdeaef840b34351eaa61862507aeed8b
aiyamia <2272682098@qq.com>
2021-07-20 01:17:25 +0800

    完成不带存储的基本的功能

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # fangpian_chrome
-方片，纯粹的单词卡片 A Chrome extension for Chinese to learn english words
+方片，纯粹的单词卡片（Chrome扩展） A Chrome extension for Chinese to learn english words
+
+
+
+本项目是**《方片》**系列的第一个正式作品，是一个Chrome浏览器扩展（俗称”插件“）
+
+
+
+## 安装方法（Installation）
+
+1. 在Chrome浏览器里地址栏输入`chrome://extensions/` 回车，来到扩展页面。
+
+2. 确保右上角的**开发者模式**处于打开状态，如下图：
+![image-20210720002121286](README.assets/image-20210720002121286.png)
+
+3. 从 [这里](https://github.com/aiyamia/fangpian_chrome/releases/latest) 下载最新版压缩包，解压之后，将`README.md`文件上一级的**fangpian_chrome**文件夹用鼠标拖入上面这个扩展页面，就会自动完成安装。
+
+4. 如果安装之后你在浏览器最上方找不到这个扩展，它可能被折叠了。你需要点击下图这个标志展开扩展程序栏
+![image-20210720002911380](README.assets/image-20210720002911380.png)
+然后找到方片，如下图点击按钮将其**固定**。
+![image-20210720003053751](README.assets/image-20210720003053751.png)
+这样它就会待在你的Chrome浏览器上方了。
+![image-20210720004358789](README.assets/image-20210720004358789.png)
+
+
+
+## 开始使用（Getting Started）
+
+来到含有你所要了解含义的词的任意页面，按住ctrl（Mac用户是cmd）键，用鼠标点击这个词，片刻之后会出现这个词的音标和释义。
+![image-20210720004931743](README.assets/image-20210720004931743.png)
+
+当你需要关闭这个卡片的时候，只需要点击一下卡片之外的页面。
+![20210720_005500](README.assets/20210720_005500.gif)
+
+
+
+## 不止这些（There will be more）
+
+本项目伊始，正在为其添加记录数据的能力，未来会让用户查看自己的单词卡片。
+
+
+
+## 欢迎你的想法和体验（Your ideas and experiences are wellcome）
+
+你在使用中的任何想法都会被作者认真考虑。也欢迎和我一起来开发~
---
+++ js/content-script.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+  $(document.body).bind('mousedown', function (e) {
+    if (e.ctrlKey || macKeys.cmdKey) {
+      $(document.body).css("cursor", "pointer");
+      var s = window.getSelection();
+      s.removeAllRanges();//取消选择
+      $(".FangPianCard").remove();
+    }else{
+      $(this).trigger('onmousedown');
+    }
+  });
+  $(document.body).bind('mouseup', async function (e) {
+    if (e.ctrlKey || macKeys.cmdKey) {
+      $(document.body).css("cursor", "initial");
+      var s;
+      if (window.getSelection) {
+        s = window.getSelection();
+      } else if (document.selection) {
+        s = document.selection.createRange();
+      }
+      var range = s.getRangeAt(0);
+      var node = s.anchorNode;
+      while (range.startOffset > 0 && /[a-zA-Z]/.test(range.toString()[0])) {
+        range.setStart(node, (range.startOffset - 1));
+      }
+      if (range.startOffset != 0)
+        range.setStart(node, range.startOffset + 1);
+      do {
+        range.setEnd(node, range.endOffset + 1);
+      }
+      while (/[a-zA-Z]/.test(range.toString().slice(-1)));
+      range.setEnd(node, range.endOffset - 1);
+      var word = range.toString().trim();
+      if (word != '') {
+        $card = await getCardNode(word);
+        $card.css({
+          "top": e.pageY,
+          "left": e.pageX
+        });
+        $card.appendTo('body');
+        window.addEventListener("click", onClickOutside);
+      }
+    }else{
+      $(this).trigger('onmouseup');
+    }
+  });
+  const getCardNode = async(word)=>{
+    const data = await fetchData(word);
+    $card = $('<div class="FangPianCard"></div>');
+    $pron = $('<div class="pronunciation"></div>');
+    $mean = $('<div class="meaning"></div>');
+    data.meaning.forEach(element => {
+      const mean_str = element.ps+ " " + element.exp.join("；");
+      $mean.append( `<p>${mean_str}</p>` );
+    });
+    $pron.append( `/${data.pronunciation.BrE}/` );
+    $card.append($pron);
+    $card.append($mean);
+    return $card;
+  };
+  const fetchData = async(word)=>{
+    let response = await fetch(`https://dictweb.translator.qq.com/api/elementary?word=${word}`);
+    let result = await response.json();
+    let data = {
+      meaning:result.oxford_dict_info.abstract,
+      pronunciation:result.oxford_dict_info.ph_json
+    };
+    return data;
+  };
+  const onClickOutside = (e) => {
+    if (!$(e.target).closest('.FangPianCard').length) {
+      var s = window.getSelection();
+      s.removeAllRanges();//取消选择
+      $(".FangPianCard").remove();
+      window.removeEventListener("click", onClickOutside);
+    }
+  };
+});
